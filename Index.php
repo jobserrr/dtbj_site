768734b33e16ba3588fb32aae7ea7c78e1dca837
Bob-137 <85346136+Bob-137@users.noreply.github.com>
2021-06-08 09:39:53 +0200

    Aanpassingen Index en styleIndex
    Index en styleIndex

--- Index.php
+++ Index.php
@@ -15,31 +15,31 @@
 </head>
 
 <body>
-    <div class="mainContainerindex">
-        <div id="myCarousel" class="carousel slide carousel-fade relative-Index" data-bs-ride="carousel">
+    <div class="mainContainerindex relative-Index">
+        <div id="myCarousel" class="carousel slide carousel-faderelative-Index" data-bs-ride="carousel">
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img src="images/forest1.jpg" class="d-block w-100" alt="...">
+                    <img src="img/forest1.jpg" class="d-block w-100 h-100vh" alt="...">
                 </div>
                 <div class="carousel-item">
-                    <img src="images/forest2.jpg" class="d-block w-100" alt="...">
+                    <img src="img/forest2.jpg" class="d-block w-100 h-100vh" alt="...">
                 </div>
                 <div class="carousel-item">
-                    <img src="images/forest3.jpg" class="d-block w-100" alt="...">
+                    <img src="img/forest3.jpg" class="d-block w-100 h-100vh" alt="...">
                 </div>
             </div>
         </div>
-        <ul class="nav absolute-Index">
-            <li class="nav-item button-Color-Index">
-                <a class="nav-link active" aria-current="page" href="Register.html">Active</a>
-            </li>
-        </ul>
     </div>
-    <!-- test -->
+    <a href="Register.html" class="loginButton">
+      <input type="button" value="Register"></button>
+ </a>
+ <a href="Login.html" class="loginButton">
+      <input type="button" value="Login"></button>
+ </a>
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
-    
+
 </body>
 
 </html>
