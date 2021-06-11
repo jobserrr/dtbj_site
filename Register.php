e9c79476d307871322f35ea46e6c8078c8a5a98f
jobserrr <86053@roc-teraa.nl>
2021-06-11 13:35:50 +0200

    update

--- Register.php
+++ Register.php
@@ -8,7 +8,8 @@
     <title>Document</title>
     <link rel="icon" href="images/favicon.png" type="image/png" sizes="16x16">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">    <script type="text/javascript" src="java/java.js"></script>
+
 </head>
 
 <body>
