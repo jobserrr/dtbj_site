e5d965d58d0492d6f7425616c1170ced006a75e8
DaanRijnders <85405@roc-teraa.nl>
2021-06-24 12:20:22 +0200

    Update functions.php

--- inc/functions.php
+++ inc/functions.php
@@ -20,4 +20,61 @@ function getPost()
     // return the array
     return $posts;
 }
+function storeMessage()
+{
+    if($_SERVER['REQUEST_METHOD'] == "POST")
+    {
+        // fetch posted data
+        $userMessage        = trim($_POST['userMessage']);
+        
+        $errorMessage = [];
+        // Empty first name
+        if($userMessage == "")
+        {
+            // add new item to the array $error
+            $errorMessage[] = "Please leave a valid message";
+        }
+        elseif(strlen($userMessage) > 160)
+        {
+            $errorMessage[] = "The message provided is too long, max 160 characters permitted!";
+        }
+        // count the number of items in $errorMessage
+        if(count($errorMessage) > 0)
+        {
+            // errors occurred
+            // don't do anything
+            // just return an error or errors
+            return $errorMessage;
+        }
+        else // if count($errorMessage) == 0
+        {
+            $conn = db_connect();
+            // define the query to be sent
+            $insetPostSQL = "
+            INSERT INTO `post`
+            (
+                `postId`,
+                `postMessage`
+            )
+            VALUES
+            (
+                '',
+                '" . $userMessage . "'
+            )
+            ";
+           
+            // run / execute the query and store in result
+            // if boolean true, then the SQL passed
+            $result = $conn->query($insetPostSQL) or die($conn->error);
+        }
+        if($result)
+        {
+            return array("Thnx for filling in the form", "We look out to your next message!");
+        }
+
+        return false;
+    }
+    return "Please fill in our Guestbook";
+}
 ?> 
+
