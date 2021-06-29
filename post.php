a36a9f1d4c6c01c3bd716d14292043643deb9128
DaanRijnders <85405@roc-teraa.nl>
2021-06-29 12:09:35 +0200

    bla

--- post.php
+++ post.php
@@ -50,7 +50,7 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
             <span class="tooltip">Account</span>
           </li>
           <li>
-            <a href="#">
+            <a href="Settings.php">
               <i class='bx bx-cog'></i>
               <span class="links_name">Settings</span>
             </a>
@@ -62,8 +62,8 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
             <div class="profile_details">
               <img src="images/profielfoto.png" alt="PF">
               <div class="name_email">
-                <div class="name">Jesper Pluijm</div>
-                <div class="email">86053@roc-teraa.nl</div>
+                  <div class="name"><?php echo $_SESSION['name']  ?></div>
+                  <div class="email">@<?php echo $_SESSION['user_name']?></div>
               </div>
             </div>
             <a class="logout" href="logout.php"><i class='bx bx-log-out' id="log_out"></i></a>
@@ -71,58 +71,10 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
         </div>
       </div>
       <div class="home_content">
-      <!-- <div class="home_container">
-        <div class="homemessage_container" id="MessageContainer">
-          <div class="homemessageheader">
-            <div class="home-profilepictures">
-              <img src="images/profielfoto.png" alt="Profile Picture">
-            </div>
-            <div class="home-profilename">
-            <h4 class="home-username">DaanRijnders (username)</h4>
-            </div>
-          </div>
-          <div class="home-messagebox">
-            <img src="images/logo.png" alt="">
-            <p>Hallo Mensen!
-              Welkom bij mijn eerste post. Ik ga vandaag aanslag plegen op het huis van
-              jesper pluijm. Ik hoop dat ik zijn hele huis opblaas. Fijne dag allemaal project X in gemert
-            </p>
-          </div>
-          <div class="homemessagefooter">
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-            <i onclick="myFunction(this)" class="fa fa-thumbs-up"></i>
-          </div>
-        </div>
-      </div>
-      <div class="home_container">
-        <div class="homemessage_container">
-          <div class="homemessageheader">
-            <div class="home-profilepictures">
-              <img src="images/profielfoto.png" alt="Profile Picture">
-            </div>
-            <div class="home-profilename">
-            <h4 class="home-username">DaanRijnders (username)</h4>
-            </div>
-          </div>
-          <div class="home-messagebox">
-            <img src="images/logo.png" alt="">
-            <p>Hallo Mensen!
-              Welkom bij mijn eerste post. Ik ga vandaag aanslag plegen op het huis van
-              jesper pluijm. Ik hoop dat ik zijn hele huis opblaas. Fijne dag allemaal project X in gemert
-            </p>
-          </div>
-          <div class="homemessagefooter">
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-            <i onclick="myFunction(this)" class="fa fa-thumbs-up"></i>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div> -->
   <?php
     $sname = "localhost";
     $uname= "root";
-    $password="usbw";
+    $password="";
     $db_name = "dtbj_database";
 
     $conn = mysqli_connect($sname, $uname, $password, $db_name);
