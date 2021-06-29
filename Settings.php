b07fda8870d3a856ad68ba226d62c8dca3d3c07a
sjihThijs <75676156+sjihThijs@users.noreply.github.com>
2021-06-29 10:00:46 +0200

    wachtwoord veranderen

--- Settings.php
+++ Settings.php
@@ -1,3 +1,9 @@
+<?php 
+session_start();
+
+if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
+
+ ?>
 <!DOCTYPE html>
 <html lang="en-US">
 
@@ -91,7 +97,81 @@
                   <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
                     <div class="form-group">
                       <label for="phone">Password</label>
-                      <input type="text" class="form-control" id="phone" placeholder="Password">
+                      <br>
+                    <!-- Trigger/Open The Modal -->
+                    <button id="myBtn">Change password</button>
+
+                    <!-- The Modal -->
+                    <div id="myModal" class="modal">
+
+                      <!-- Modal content -->
+                      <div class="modal-content">
+                        <span class="close">&times;</span>
+                        <form action="change-p.php" method="post">
+                        <h2>Change Password</h2>
+                        <?php if (isset($_GET['error'])) { ?>
+                          <p class="error"><?php echo $_GET['error']; ?></p>
+                        <?php } ?>
+
+                        <?php if (isset($_GET['success'])) { ?>
+                              <p class="success"><?php echo $_GET['success']; ?></p>
+                          <?php } ?>
+
+                        <label>Old Password</label>
+                        <input type="password" 
+                              name="op" 
+                              placeholder="Old Password">
+                              <br>
+
+                        <label>New Password</label>
+                        <input type="password" 
+                              name="np" 
+                              placeholder="New Password">
+                              <br>
+
+                        <label>Confirm New Password</label>
+                        <input type="password" 
+                              name="c_np" 
+                              placeholder="Confirm New Password">
+                              <br>
+                        <a href="home.php" class="ca">
+                        <button type="submit">Change</button>
+                          </a>
+                      </form>
+                      </div>
+
+                    </div>
+
+                    <script>
+                    // Get the modal
+                    var modal = document.getElementById("myModal");
+
+                    // Get the button that opens the modal
+                    var btn = document.getElementById("myBtn");
+
+                    // Get the <span> element that closes the modal
+                    var span = document.getElementsByClassName("close")[0];
+
+                    // When the user clicks the button, open the modal 
+                    btn.onclick = function() {
+                      modal.style.display = "block";
+                    }
+
+                    // When the user clicks on <span> (x), close the modal
+                    span.onclick = function() {
+                      modal.style.display = "none";
+                    }
+
+                    // When the user clicks anywhere outside of the modal, close it
+                    window.onclick = function(event) {
+                      if (event.target == modal) {
+                        modal.style.display = "none";
+                      }
+                    }
+                    </script>
+
+                    </body>
+                    </html>
                     </div>
                   </div>
                   <div class="row gutters">
@@ -212,3 +292,10 @@
 </body>
 
 </html>
+
+<?php 
+}else{
+     header("Location: index.php");
+     exit();
+}
+ ?>
